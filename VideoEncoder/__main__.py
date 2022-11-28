72c435f91b059fc5dda7e01be88427c5770756c2
NkBots <116135526+NkBots@users.noreply.github.com>
2022-11-28 23:12:57 +0530

    Update __main__.py

--- VideoEncoder/__main__.py
+++ VideoEncoder/__main__.py
@@ -5,10 +5,13 @@ import os
 from web import web_server
 from . import app, log
 
+from aiohttp import web
 
-from flask import Flask
+from aio_tiny_healthcheck.checker import Checker
 
-cck = Flask(__name__)
+#from flask import Flask
+
+#cck = Flask(__name__)
 
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers = [
@@ -18,11 +21,11 @@ port = os.environ.get("PORT", "8080")
 
 async def main():
     await app.start()
-    tech = web.AppRunner(await web_server())
-    await tech.setup()
-    bind_address = "0.0.0.0"
+    #tech = web.AppRunner(await web_server())
+   # await tech.setup()
+   # bind_address = "0.0.0.0"
 
-    await web.TCPSite(tech, bind_address, port).start()
+    #await web.TCPSite(tech, bind_address, port).start()
     await app.send_message(chat_id=log, text=f'<b>Bot Started! @{(await app.get_me()).username}</b>')
     await idle()
     await app.stop()
@@ -30,6 +33,18 @@ async def main():
 app.loop.run_until_complete(main())
 
 
-@cck.route("/health")
-def health():
-    return
+
+def some_sync_check():
+    return True
+
+async def some_async_check():
+    return False
+
+healthcheck_provider = Checker()
+healthcheck_provider.add_check('sync_check_true', some_async_check)
+healthcheck_provider.add_check('async_check_false', some_async_check)
+
+
+app = web.Application()
+app.router.add_get('/healthcheck', healthcheck_provider.aiohttp_handler)
+web.run_app(app)
